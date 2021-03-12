d9bc416daec9edf906b0435d685021e89ee2b6ce
Sergiy Poddubka <poseal@gmail.com>
2021-03-12 11:03:07 +0200

    Added new controllers with mapping

---
+++ src/main/java/ua/com/poseal/springcourse/controllers/FirstController.java
@@ -0,0 +1,19 @@
+package ua.com.poseal.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage() {
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodBeyPage() {
+        return "first/goodbye";
+    }
+}
