809bc6ce42caf75f4ca482bd0456c057099030a3
Sergiy Poddubka <poseal@gmail.com>
2021-03-12 15:35:02 +0200

    Get request param by @RequestParam

--- src/main/java/ua/com/poseal/springcourse/controllers/FirstController.java
+++ src/main/java/ua/com/poseal/springcourse/controllers/FirstController.java
@@ -2,16 +2,14 @@ package ua.com.poseal.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam("name") String name,
+                            @RequestParam("surname") String surname) {
 
         System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
